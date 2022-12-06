95ea9ebbf17371143b7b949ba8bb6c167ddd8db0
seitel1997 <119934543+seitel1997@users.noreply.github.com>
2022-12-06 12:58:40 +0100

    Update test.py

--- test.py
+++ test.py
@@ -9,11 +9,6 @@ import numpy as np
 
 st.title('BONJORNO')
 
-sel1=st.selctbox('Selection', [1,2,3])
-st.write('Deine Auswahl ist: ', sel1)
-
-x=np.linspace(0,50,50)
-plt.plot(x, X**2)
 
 arr = np.random.normal(1, 1, size=100)
 fig, ax = plt.subplots()
