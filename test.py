eaa46029bc01ffa9acd80c20888b3f9776eee441
seitel1997 <119934543+seitel1997@users.noreply.github.com>
2022-12-06 13:20:34 +0100

    Update test.py

--- test.py
+++ test.py
@@ -12,6 +12,11 @@ import pandas as pd
 st.title('BONJORNO')
 
 df=pd.read_csv('Bastar Craton.csv')
-df
 
-plt.scatter(data['MGO(WT%)'],data['SIO2(WT%)'])
+el1 ='MnO'
+el2 = 'SiO2'
+plt.scatter(dfM[el1],df[el2], label='MinData')
+plt.xlabel(el1)
+plt.ylabel(el2)
+plt.legend(loc='lower right')
+plt.show()
