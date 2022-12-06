5b39fb8ae5594e8b6dbd976c69683c2d1be0d568
seitel1997 <119934543+seitel1997@users.noreply.github.com>
2022-12-06 12:51:45 +0100

    Update test.py

--- test.py
+++ test.py
@@ -7,6 +7,10 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 
+st.title('BONJORNO')
+
+
+
 arr = np.random.normal(1, 1, size=100)
 fig, ax = plt.subplots()
 ax.hist(arr, bins=20)
