50cf31a33f0f63aa6a6daaa192535112aefed1e6
seitel1997 <119934543+seitel1997@users.noreply.github.com>
2022-12-06 13:06:47 +0100

    Update test.py

--- test.py
+++ test.py
@@ -6,10 +6,9 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+
 
 st.title('BONJORNO')
 
-option = st.selectbox(
-    'wie geht´s?',
-    ('gut', 'mäh', 'schlecht'))
 
